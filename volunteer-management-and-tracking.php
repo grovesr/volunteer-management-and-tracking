814c7789d69edeecfee95e78663f0acb4cccc70e
Rob <robgroves0.gmailcom>
2020-04-27 08:40:56 -0400

    fix: allow php version below 7.3

--- volunteer-management-and-tracking.php
+++ volunteer-management-and-tracking.php
@@ -5,7 +5,7 @@
  Description:       Handle the basics with this plugin.
  Version:           0.0.0
  Requires at least: 5.3
- Requires PHP:      7.2
+ Requires PHP:      5.3
  Author:            Rob Groves
  Author URI:        https://author.example.com/
  License:           GPL v2 or later
